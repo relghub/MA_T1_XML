6270e21486803be2e5a714e1832528964af1feb8
wideprism <winsvintus@gmail.com>
2024-10-27 20:48:47 +0100

    * Merged 'regex-test' with 'master' branch

--- app/src/main/java/com/example/pleasesignin/MainActivity.kt
+++ app/src/main/java/com/example/pleasesignin/MainActivity.kt
@@ -16,12 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(LoginFragment())
