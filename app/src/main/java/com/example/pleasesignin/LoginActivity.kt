217b9a0c62cf2897910876a2ec7bbc1092c5e5c5
wideprism <winsvintus@gmail.com>
2024-11-06 17:41:24 +0100

    * Implemented switching between login and register activities * Removed constraint bias on some TextView components

---
+++ app/src/main/java/com/example/pleasesignin/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.pleasesignin
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val callToRegisterLabel = findViewById<TextView>(R.id.login_to_register)
+
+        callToRegisterLabel.setOnClickListener {
+            Log.d("Onboarding", "Register now label pressed")
+
+            val goToRegisterIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(goToRegisterIntent)
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/pleasesignin/LoginFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pleasesignin
-
-import androidx.fragment.app.Fragment
-
-class LoginFragment : Fragment(R.layout.activity_login) {
-}
