cdcd19e3f7976675cb7a74c2ca938468160110be
wideprism <winsvintus@gmail.com>
2024-11-06 16:27:14 +0100

    * Redundant code removed

--- app/src/main/java/com/example/pleasesignin/MainActivity.kt
+++ app/src/main/java/com/example/pleasesignin/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.pleasesignin
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.pleasesignin.databinding.ActivityMainBinding
 
@@ -22,9 +20,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager;
-        val fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
         fragmentTransaction.commit()
     }
 }
