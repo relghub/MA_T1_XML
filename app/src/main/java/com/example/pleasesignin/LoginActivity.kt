a8c737de154d0e0e545de39352e946137a58f900
ozanba <ozanbagirann@gmail.com>
2024-11-20 17:47:56 +0100

    error managment

--- app/src/main/java/com/example/pleasesignin/LoginActivity.kt
+++ app/src/main/java/com/example/pleasesignin/LoginActivity.kt
@@ -3,11 +3,15 @@ package com.example.pleasesignin
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlin.toString
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,37 @@ class LoginActivity : AppCompatActivity() {
 
         val callToRegisterLabel = findViewById<TextView>(R.id.login_to_register)
 
-        callToRegisterLabel.setOnClickListener {
+        val inputEmail = findViewById<TextInputLayout>(R.id.register_email_field)
+        val inputPassword = findViewById<TextInputLayout>(R.id.register_password_field)
+
+        val inputEmailText = findViewById<TextInputEditText>(R.id.register_email_field_Text)
+        val inputPasswordText = findViewById<TextInputEditText>(R.id.register_password_field_Text)
+
+        val nextButton = findViewById<Button>(R.id.button)
+        nextButton.setOnClickListener {
+            val email = inputEmailText.text.toString()
+            val password = inputPasswordText.text.toString()
+
+            val isEmailValid = CredentialsManager().isEmailValid(email)
+            val isPasswordValid = CredentialsManager().isPasswordValid(password)
+
+            if (!isEmailValid) {
+                inputEmail.isErrorEnabled = true
+                inputEmail.error = "Email is invalid!"
+            } else {
+                inputEmail.isErrorEnabled = false
+            }
+
+            if (!isPasswordValid) {
+                inputPassword.isErrorEnabled = true
+                inputPassword.error = "Password is invalid!"
+            } else {
+                inputPassword.isErrorEnabled = false
+            }
+
+
+    }
+            callToRegisterLabel.setOnClickListener {
             Log.d("Onboarding", "Register now label pressed")
 
             val goToRegisterIntent = Intent(this, RegisterActivity::class.java)
